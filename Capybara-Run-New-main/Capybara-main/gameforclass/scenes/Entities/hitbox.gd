9b0ad4bfa83bbe51619d4a64805e12f2d40e26b4
Siricepick <167460828+Siricepick@users.noreply.github.com>
2025-05-16 09:47:31 -0700

    Delete Capybara-Run-New-main/Capybara-main directory

--- Capybara-Run-New-main/Capybara-main/gameforclass/scenes/Entities/hitbox.gd
+++
@@ -1,2 +0,0 @@
-class_name Hitbox
-extends Area2D
--- Capybara-Run-New-main/Capybara-main/gameforclass/scenes/levels/level_1.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@export var level_id : int
-@export var level_start_pos : Node2D
-
-func _ready() -> void:
-	LevelManager.main_scene = _2d_scene
-	LevelManager.levels = available_levels
