20072b08382e60f76a0a7d6154d372d5d8d49a3b
Siricepick <167460828+Siricepick@users.noreply.github.com>
2025-05-16 10:03:09 -0700

    Delete Capybara-main directory

--- Capybara-main/gameforclass/main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@export var available_levels: Array[LevelData]
-
-@onready var _2d_scene = $"2DScrene"
-
-func _ready() -> void:
-	LevelManager.main_scene = _2d_scene
-	LevelManager.levels = available_levels
