7b0f699fb78b3ad36b63fd4d431ced9988d91e09
DuelStix <cyrusgarroway88@gmail.com>
2025-05-22 08:50:38 -0700

    Add files via upload

--- Capybara-main/gameforclass/main.gd
+++ Capybara-main/gameforclass/main.gd
@@ -9,49 +9,25 @@ func _ready() -> void:
 	LevelManager.levels = available_levels
 
 func _on_pause_menu_game_paused() -> void:
-	var Spiders
-	if $"2DScrene"/Level/Spiders != null:
-		Spiders = $"2DScrene"/Level/Spiders.get_children()
-	else:
-		Spiders = null
-		
-	var Jaguars
-	if $"2DScrene"/Level/Jaguars != null:
-		Jaguars = $"2DScrene"/Level/Jaguars.get_children()
-	else:
-		Jaguars = Array()
-	
-	if Spiders != null:
-		for v in Spiders:
-			v.speed = 0
-			
-	if Jaguars != null:
-		for v in Jaguars:
-			v.speed = 0
+	var Spiders = $"2DScrene"/Level/Spiders.get_children()
+	var Jaguars = $"2DScrene"/Level/Jaguars.get_children()
+	$"2DScrene"/Level/Player.SPEED = 0
+	for v in Spiders:
+		v.speed = 0
+	for v in Jaguars:
+		v.speed = 0
 
 func _on_pause_menu_game_resumed() -> void:
-	var Spiders
-	if $"2DScrene"/Level/Spiders != null:
-		Spiders = $"2DScrene"/Level/Spiders.get_children()
-	else:
-		Spiders = []
-		
-	var Jaguars
-	if $"2DScrene"/Level/Jaguars != null:
-		Jaguars = $"2DScrene"/Level/Jaguars.get_children()
-	else:
-		Jaguars = Array()
-	
-	if Spiders != null:
-		for v in Spiders:
-			if v.moving_down:
-				v.speed = 60
-			else:
-				v.speed = -60
-
-	if Jaguars != null:
-		for v in Jaguars:
-			if v.facing_right:
-				v.speed = 60
-			else:
-				v.speed = -60
+	var Spiders = $"2DScrene"/Level/Spiders.get_children()
+	var Jaguars = $"2DScrene"/Level/Jaguars.get_children()
+	$"2DScrene"/Level/Player.SPEED = 220
+	for v in Spiders:
+		if v.moving_down:
+			v.speed = 60
+		else:
+			v.speed = -60
+	for v in Jaguars:
+		if v.facing_right:
+			v.speed = 60
+		else:
+			v.speed = -60
